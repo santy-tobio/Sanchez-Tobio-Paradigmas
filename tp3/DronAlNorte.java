f79afd978540b64306207d630d23e440093dbb8e
juanjo <juanjo@mbp-de-juanjo.home>
2024-05-22 08:36:45 -0300

    elimino archivos de sobra

--- tp3/DronAlNorte.java
+++
@@ -1,9 +0,0 @@
-package drones;
-
-public class DronAlNorte extends DronSondaNoDesplegada{
-
-    public DronAlNorte(int velocidad, String direccion, boolean sondaDesplegada) {
-        super(velocidad, direccion, sondaDesplegada);
-        this.direccion = "N";
-    }
-}
