3c27d3f410d57e78925c5d4a76107ccefd12c332
juanjoudesa <141250002+juanjoudesa@users.noreply.github.com>
2024-06-14 16:01:14 -0300

    Borrar archivos basura

--- tp4/uno/UnoTest.java
+++ tp4/uno/UnoTest.java
@@ -1,6 +1,6 @@
 package uno;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import java.util.*;
 import org.junit.jupiter.api.function.Executable;
 import static org.junit.jupiter.api.Assertions.*;
